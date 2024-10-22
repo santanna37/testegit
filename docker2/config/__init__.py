5dd697d222a3d5beb49415d839b0f943b1a16abc
Luiz Bernardo <luiz.bernardo.ext@ipiranga.ipiranga>
2024-10-22 14:31:55 -0300

    c0: config - Iniciando atividade , retirando residuos indesejaveis

--- docker2/config/__init__.py
+++
@@ -1,1 +0,0 @@
-# -docker run -d --name banco-docker -e POSTGRES_USER=root -e POSTGRES_PASSWORD=password -p 5543:5543 postgres:latest
--- docker2/models/models.py
+++
@@ -1,9 +0,0 @@
-# BIBLIOTECAS 
-from sqlalchemy import Column, Integer, String
-from config.database import Base
-
-
-class Usuario(Base):
-    __tablename__='usuario'
-    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
-    nome = Column(String)
