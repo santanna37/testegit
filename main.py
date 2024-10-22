34ae8556dd8f4d3abf68fb7fb995134b526f1b67
Luiz Bernardo <luiz.bernardo.ext@ipiranga.ipiranga>
2024-10-22 15:00:00 -0300

    c1 - primeiro commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+display_massages = [
+    "seja feliz",
+    "fique bem"
+]
+
+while True:
+    resposta = input( "Deseja receber um conselho ? s/n:  ")
+    if resposta == 's' or resposta =='S':
+        menssagem = random.choice(display_massages)
+        print(menssagem)
+        print('===========')
+
